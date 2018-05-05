bd279f4fc95933ef476b577a850a79971110fed2
linus123 <linus_pagan@hotmail.com>
2018-05-05 07:20:01 -0500

    Added data page object

--- src/Core/BplusTreeDataStructure/BplusTree.cs
+++ src/Core/BplusTreeDataStructure/BplusTree.cs
@@ -1,95 +1,28 @@
-﻿using System.Collections.Generic;
-
-namespace ThreadingExplore.Core.BplusTreeDataStructure
+﻿namespace ThreadingExplore.Core.BplusTreeDataStructure
 {
     public class BplusTree
     {
-        private CustomerRecord[] _dataPage;
-        private readonly int _pageSize;
+        private DataPage _dataPage;
 
         public BplusTree(
             int pageSize = 2)
         {
-            _pageSize = pageSize;
-            _dataPage = new CustomerRecord[pageSize + 1];
+            _dataPage = new DataPage(pageSize);
         }
 
         public void Insert(CustomerRecord customerRecord)
         {
-            if (IsDataPageFull(_dataPage))
+            var isFull = _dataPage.Insert(customerRecord);
+
+            if (isFull)
             {
-                for (int pageIndex = 0; pageIndex < _pageSize - 1; pageIndex++)
-                {
-                    if (_dataPage[pageIndex].CustomerId > customerRecord.CustomerId)
-                    {
-                        _dataPage = InsertAndShift(_dataPage, customerRecord, pageIndex);
-
-                        // Split
-
-                        return;
-                    }
-                }
+                // split
             }
-            else
-            {
-                for (int pageIndex = 0; pageIndex < _pageSize; pageIndex++)
-                {
-                    if (_dataPage[pageIndex] == null)
-                    {
-                        _dataPage[pageIndex] = customerRecord;
-
-                        return;
-                    }
-
-                    if (_dataPage[pageIndex].CustomerId > customerRecord.CustomerId)
-                    {
-                        _dataPage = InsertAndShift(_dataPage, customerRecord, pageIndex);
-
-                        return;
-                    }
-                }
-            }
-
         }
 
         public CustomerRecord[] GetAll()
         {
-            var customerRecords = new List<CustomerRecord>();
-
-            foreach (var customerRecord in _dataPage)
-            {
-                if (customerRecord != null)
-                    customerRecords.Add(customerRecord);
-            }
-
-            return customerRecords.ToArray();
-        }
-
-        private CustomerRecord[] InsertAndShift(
-            CustomerRecord[] dataPage,
-            CustomerRecord customerRecord,
-            int insertIndex)
-        {
-            for (int indexCounter = insertIndex; indexCounter < (_pageSize - 1); indexCounter++)
-            {
-                dataPage[insertIndex + 1] = dataPage[insertIndex];
-            }
-
-            dataPage[insertIndex] = customerRecord;
-
-            return dataPage;
-        }
-
-        private bool IsDataPageFull(
-            CustomerRecord[] dataPage)
-        {
-            for (int i = 0; i < _pageSize - 1; i++)
-            {
-                if (dataPage[i] == null)
-                    return false;
-            }
-
-            return true;
+            return _dataPage.GetAll();
         }
     }
 
---
+++ src/Core/BplusTreeDataStructure/DataPage.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThreadingExplore.Core.BplusTreeDataStructure
+{
+    public class DataPage
+    {
+        private CustomerRecord[] _dataPage;
+
+        private readonly int _pageSize;
+
+        public DataPage(
+            int pageSize)
+        {
+            _pageSize = pageSize;
+            _dataPage = new CustomerRecord[pageSize + 1];
+        }
+
+        public bool Insert(
+            CustomerRecord customerRecord)
+        {
+            if (IsFull())
+                return false;
+
+            for (int pageIndex = 0; pageIndex < _pageSize; pageIndex++)
+            {
+                if (_dataPage[pageIndex] == null)
+                {
+                    _dataPage[pageIndex] = customerRecord;
+
+                    return true;
+                }
+
+                if (_dataPage[pageIndex].CustomerId > customerRecord.CustomerId)
+                {
+                    _dataPage = InsertAndShift(_dataPage, customerRecord, pageIndex);
+
+                    return true;
+                }
+            }
+
+            throw new Exception("Something bad happend");
+        }
+
+        private bool IsFull()
+        {
+            for (int i = 0; i < _pageSize; i++)
+            {
+                if (_dataPage[i] == null)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private CustomerRecord[] InsertAndShift(
+            CustomerRecord[] dataPage,
+            CustomerRecord customerRecord,
+            int insertIndex)
+        {
+            for (int indexCounter = insertIndex; indexCounter < (_pageSize - 1); indexCounter++)
+            {
+                dataPage[insertIndex + 1] = dataPage[insertIndex];
+            }
+
+            dataPage[insertIndex] = customerRecord;
+
+            return dataPage;
+        }
+
+        public CustomerRecord[] GetAll()
+        {
+            var customerRecords = new List<CustomerRecord>();
+
+            foreach (var customerRecord in _dataPage)
+            {
+                if (customerRecord != null)
+                    customerRecords.Add(customerRecord);
+            }
+
+            return customerRecords.ToArray();
+        }
+
+
+    }
+}
