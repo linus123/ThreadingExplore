6b2a89f5fa520e2c33c491259ffb2283b25cff06
linus123 <linus_pagan@hotmail.com>
2018-05-05 06:23:32 -0500

    Added more tests

--- src/UnitTests/BplusTreeDataStructure/BplusTreeTests.cs
+++ src/UnitTests/BplusTreeDataStructure/BplusTreeTests.cs
@@ -45,5 +45,28 @@ namespace ThreadingExplore.UnitTests.BplusTreeDataStructure
             customers[1].Name.Should().Be("Customer 101");
         }
 
+        [Fact]
+        public void ShouldWorkWithTwoDataPagesOutOfOrder()
+        {
+            var tree = new BplusTree();
+
+            var cusomter02 = new CustomerRecord(101, "Customer 101");
+            tree.Insert(cusomter02);
+
+            var customer01 = new CustomerRecord(100, "Customer 100");
+            tree.Insert(customer01);
+
+            var customers = tree.GetAll();
+
+            customers.Should().HaveCount(2);
+
+            customers[0].CustomerId.Should().Be(100);
+            customers[0].Name.Should().Be("Customer 100");
+
+            customers[1].CustomerId.Should().Be(101);
+            customers[1].Name.Should().Be("Customer 101");
+        }
+
+
     }
 }
