ea6b3dd04cfc12aef51400d57a374d99e9bfc51b
linus123 <linus_pagan@hotmail.com>
2018-05-05 08:13:32 -0500

    Split classes into files

---
+++ src/Core/BplusTreeDataStructure/IndexPage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace ThreadingExplore.Core.BplusTreeDataStructure
+{
+    public class IndexPage
+    {
+        private int _pageSize;
+        private int[] _indexes;
+        private DataPage[] _dataPages;
+
+        public IndexPage(
+            int pageSize,
+            DataPage dataPage1,
+            DataPage dataPage2)
+        {
+            _pageSize = pageSize;
+
+            _indexes = new int[pageSize];
+            _dataPages = new DataPage[pageSize + 1];
+
+            _dataPages[0] = dataPage1;
+            _dataPages[1] = dataPage2;
+        }
+
+        public CustomerRecord[] GetAll()
+        {
+            var customerRecords = new List<CustomerRecord>();
+
+            foreach (var dataPage in _dataPages)
+            {
+                if (dataPage == null)
+                    break;
+
+                customerRecords.AddRange(dataPage.GetAll());
+            }
+
+            return customerRecords.ToArray();
+        }
+    }
+}
---
+++ src/Core/BplusTreeDataStructure/InsertResult.cs
@@ -0,0 +1,30 @@
+﻿namespace ThreadingExplore.Core.BplusTreeDataStructure
+{
+    public class InsertResult
+    {
+        public static InsertResult CreateInsertSuccess()
+        {
+            return new InsertResult()
+            {
+                WasSuccessful = true
+            };
+        }
+
+        public static InsertResult CreateAsSplit(
+            DataPage leftDataPage,
+            DataPage rightDataPage)
+        {
+            return new InsertResult()
+            {
+                WasSuccessful = false,
+                LeftDataPage = leftDataPage,
+                RightDataPage = rightDataPage
+            };
+        }
+
+        public bool WasSuccessful { get; private set; }
+        public DataPage LeftDataPage { get; private set; }
+        public DataPage RightDataPage { get; private set; }
+
+    }
+}
