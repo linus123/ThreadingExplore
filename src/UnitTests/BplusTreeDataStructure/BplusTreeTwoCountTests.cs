eacec1c485cdc82148b2bdc5ef4c9513c3145d43
linus123 <linus_pagan@hotmail.com>
2018-05-19 06:40:56 -0500

    Added failing test for internal split

--- src/UnitTests/BplusTreeDataStructure/BplusTreeTwoCountTests.cs
+++ src/UnitTests/BplusTreeDataStructure/BplusTreeTwoCountTests.cs
@@ -34,8 +34,37 @@ namespace ThreadingExplore.UnitTests.BplusTreeDataStructure
             var customerRecord5 = CreateCustomer(140);
             tree.Insert(customerRecord5);
             tree.GetStringVersion().Should().Be("P:100|I:110|P:110|I:120|P:120|I:130|P:130|P:140");
+        }
+
+        [Fact]
+        public void InternalSplit()
+        {
+            var tree = new BplusTree(PageCount);
+
+            tree.GetStringVersion().Should().BeEmpty();
+
+            var customerRecord1 = CreateCustomer(100);
+            tree.Insert(customerRecord1);
+            tree.GetStringVersion().Should().Be("P:100");
+
+            var customerRecord2 = CreateCustomer(110);
+            tree.Insert(customerRecord2);
+            tree.GetStringVersion().Should().Be("P:100|P:110");
+
+            var customerRecord3 = CreateCustomer(120);
+            tree.Insert(customerRecord3);
+            tree.GetStringVersion().Should().Be("P:100|I:110|P:110|P:120");
+
+            var customerRecord4 = CreateCustomer(105);
+            tree.Insert(customerRecord4);
+            tree.GetStringVersion().Should().Be("P:100|P:105|I:110|P:110|P:120");
+
+            var customerRecord5 = CreateCustomer(106);
+            tree.Insert(customerRecord5);
+            tree.GetStringVersion().Should().Be("P:100|I:105|P:105|P:106|I:110|P:110|P:120");
 
         }
+
 
         [Fact]
         public void BalancedInsert()
