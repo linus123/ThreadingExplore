07fdca622c0e52f3e6698b97cc90bf6f1b1c70a1
linus123 <linus_pagan@hotmail.com>
2018-05-26 15:06:53 -0500

    Added main with million items

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using ThreadingExplore.Core;
-using ThreadingExplore.Core.DiningPhilosophers;
+﻿using System;
+using ThreadingExplore.Core.BplusTreeDataStructure;
 
 namespace ThreadingExplore.Console
 {
@@ -9,39 +7,21 @@ namespace ThreadingExplore.Console
     {
         static void Main(string[] args)
         {
-            var philosopherTable = new Butler(5);
+            var random = new Random();
 
-            var simulationLog = new SimulationLog();
 
-            var philosopher0 = new Philosopher("P0", 300, philosopherTable, 0, simulationLog);
-            var philosopher1 = new Philosopher("P1", 1000, philosopherTable, 1, simulationLog);
-            var philosopher2 = new Philosopher("P2", 10, philosopherTable, 2, simulationLog);
-            var philosopher3 = new Philosopher("P3", 30, philosopherTable, 3, simulationLog);
-            var philosopher4 = new Philosopher("P4", 10, philosopherTable, 4, simulationLog);
+            var bplusTree = new BplusTree(4);
 
-            var philosophers = new List<Philosopher>();
-            philosophers.Add(philosopher0);
-            philosophers.Add(philosopher1);
-            philosophers.Add(philosopher2);
-            philosophers.Add(philosopher3);
-            philosophers.Add(philosopher4);
-
-            var threads = new List<Thread>();
-
-            simulationLog.StartSimulation();
-
-            foreach (var philosopher in philosophers)
+            for (int i = 0; i < 1000000; i++)
             {
-                var thread = new Thread(() => philosopher.StartEating());
-                thread.Start();
+                var num = random.Next(1, 1000000);
 
-                threads.Add(thread);
+                bplusTree.Insert(new CustomerRecord(num, "Customer {num}"));
             }
 
-            foreach (var thread in threads)
-            {
-                thread.Join();
-            }
+            //var stringVersion = bplusTree.GetStringVersion();
+
+            //System.Console.WriteLine(stringVersion);
         }
     }
 }
--- src/Core/DiningPhilosophers/Butler.cs
+++ src/Core/DiningPhilosophers/Butler.cs
@@ -86,3 +86,43 @@ namespace ThreadingExplore.Core.DiningPhilosophers
         }
     }
 }
+
+/*
+ * 
+ *         static void Main(string[] args)
+        {
+            var philosopherTable = new Butler(5);
+
+            var simulationLog = new SimulationLog();
+
+            var philosopher0 = new Philosopher("P0", 300, philosopherTable, 0, simulationLog);
+            var philosopher1 = new Philosopher("P1", 1000, philosopherTable, 1, simulationLog);
+            var philosopher2 = new Philosopher("P2", 10, philosopherTable, 2, simulationLog);
+            var philosopher3 = new Philosopher("P3", 30, philosopherTable, 3, simulationLog);
+            var philosopher4 = new Philosopher("P4", 10, philosopherTable, 4, simulationLog);
+
+            var philosophers = new List<Philosopher>();
+            philosophers.Add(philosopher0);
+            philosophers.Add(philosopher1);
+            philosophers.Add(philosopher2);
+            philosophers.Add(philosopher3);
+            philosophers.Add(philosopher4);
+
+            var threads = new List<Thread>();
+
+            simulationLog.StartSimulation();
+
+            foreach (var philosopher in philosophers)
+            {
+                var thread = new Thread(() => philosopher.StartEating());
+                thread.Start();
+
+                threads.Add(thread);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+*/
