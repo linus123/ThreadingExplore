1edf74247f14de69e1f370054a9a786eb7324bb1
linus123 <linus_pagan@hotmail.com>
2018-06-11 05:39:13 -0500

    Progress

--- src/Core/TicTacToe/TicTacToeGame.cs
+++ src/Core/TicTacToe/TicTacToeGame.cs
@@ -2,16 +2,35 @@
 {
     public class TicTacToeGame
     {
+        private CellValue[,] _gameBoard;
+
         public TicTacToeGame(
             string[,] grid = null)
         {
+            _gameBoard = new CellValue[3, 3];
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    _gameBoard[x, y] = CellValue.Blank;
+                }
+            }
+        }
+
+        public void SetCellValue(
+            int x,
+            int y,
+            CellValue cellValue)
+        {
+            _gameBoard[x, y] = cellValue;
         }
 
         public CellValue GetCellValue(
             int x,
             int y)
         {
-            return CellValue.Blank;
+            return _gameBoard[x, y];
         }
 
         public enum CellValue
--- src/UnitTests/TicTacToe/TicTacToeGameTests.cs
+++ src/UnitTests/TicTacToe/TicTacToeGameTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
 using ThreadingExplore.Core.TicTacToe;
 using Xunit;
 
@@ -14,17 +16,6 @@ namespace ThreadingExplore.UnitTests.TicTacToe
             AssertAllCellsAreBlank(game);
         }
 
-        private static void AssertAllCellsAreBlank(TicTacToeGame game)
-        {
-            for (int x = 0; x < 3; x++)
-            {
-                for (int y = 0; y < 3; y++)
-                {
-                    game.GetCellValue(x, y).Should().Be(TicTacToeGame.CellValue.Blank);
-                }
-            }
-        }
-
         [Fact]
         public void ConsructorShouldInitWillAllBlankCells()
         {
@@ -38,6 +29,52 @@ namespace ThreadingExplore.UnitTests.TicTacToe
             var game = new TicTacToeGame(grid);
 
             AssertAllCellsAreBlank(game);
+        }
+
+        [Theory]
+        [ClassData(typeof(EachCellWithXandO))]
+        public void SetCellValueShouldChangeCellValues(
+            int x,
+            int y,
+            TicTacToeGame.CellValue cellValue)
+        {
+            var game = new TicTacToeGame();
+
+            game.SetCellValue(x, y, cellValue);
+            game.GetCellValue(x, y).Should().Be(cellValue);
+
+            game.SetCellValue(x, y, TicTacToeGame.CellValue.Blank);
+            game.GetCellValue(x, y).Should().Be(TicTacToeGame.CellValue.Blank);
+
+            AssertAllCellsAreBlank(game);
+        }
+
+        public class EachCellWithXandO : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    for (int y = 0; y < 3; y++)
+                    {
+                        yield return new object[] { x, y, TicTacToeGame.CellValue.X };
+                        yield return new object[] { x, y, TicTacToeGame.CellValue.O };
+                    }
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        private static void AssertAllCellsAreBlank(TicTacToeGame game)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    game.GetCellValue(x, y).Should().Be(TicTacToeGame.CellValue.Blank);
+                }
+            }
         }
     }
 }
