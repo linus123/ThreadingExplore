653e5438ee2d1dbb035c14bc3ff6cca6726f4e4b
linus123 <linus_pagan@hotmail.com>
2018-06-11 05:26:03 -0500

    Started tictactoe

---
+++ src/Core/TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,24 @@
+﻿namespace ThreadingExplore.Core.TicTacToe
+{
+    public class TicTacToeGame
+    {
+        public TicTacToeGame(
+            string[,] grid = null)
+        {
+        }
+
+        public CellValue GetCellValue(
+            int x,
+            int y)
+        {
+            return CellValue.Blank;
+        }
+
+        public enum CellValue
+        {
+            Blank,
+            X,
+            O
+        }
+    }
+}
---
+++ src/UnitTests/TicTacToe/TicTacToeGameTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using ThreadingExplore.Core.TicTacToe;
+using Xunit;
+
+namespace ThreadingExplore.UnitTests.TicTacToe
+{
+    public class TicTacToeGameTests
+    {
+        [Fact]
+        public void ConstructorShouldInitBlankGrid()
+        {
+            var game = new TicTacToeGame();
+
+            AssertAllCellsAreBlank(game);
+        }
+
+        private static void AssertAllCellsAreBlank(TicTacToeGame game)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    game.GetCellValue(x, y).Should().Be(TicTacToeGame.CellValue.Blank);
+                }
+            }
+        }
+
+        [Fact]
+        public void ConsructorShouldInitWillAllBlankCells()
+        {
+            var grid = new string[,]
+            {
+                { "---" },
+                { "---" },
+                { "---" },
+            };
+
+            var game = new TicTacToeGame(grid);
+
+            AssertAllCellsAreBlank(game);
+        }
+    }
+}
