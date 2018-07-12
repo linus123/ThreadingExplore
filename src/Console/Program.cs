468f401c34e3bd5487aa353675b55b5e76fea04d
linus123 <linus_pagan@hotmail.com>
2018-07-12 05:25:10 -0500

    On Page 560

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,29 +1,94 @@
-﻿using System.Threading;
+﻿using System.Diagnostics;
+using System.Threading;
 
 namespace ThreadingExplore.Console
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var thread = new Thread(WriteY);
+            var signal = new ManualResetEvent(false);
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
-            thread.Start();
-
-            for (int i = 0; i < 1000; i++)
+            var thread1 = new Thread(() =>
             {
-                System.Console.Write("x");
-            }
+                System.Console.WriteLine($"{stopwatch.ElapsedMilliseconds}: Thread1: Waiting for signal...");
+                signal.WaitOne();
+                System.Console.WriteLine($"{stopwatch.ElapsedMilliseconds}: Thread1: Got signal.");
+            });
 
+            var thread2 = new Thread(() =>
+            {
+                System.Console.WriteLine($"{stopwatch.ElapsedMilliseconds}: Thread2: Sleeping");
+                Thread.Sleep(3000);
+                System.Console.WriteLine($"{stopwatch.ElapsedMilliseconds}: Thread2: Waiting for signal...");
+                signal.WaitOne();
+                System.Console.WriteLine($"{stopwatch.ElapsedMilliseconds}: Thread2: Got signal.");
+            });
+
+            thread2.Start();
+            thread1.Start();
+
+            Thread.Sleep(2000);
+            signal.Set();
+
+            thread1.Join();
+            thread2.Join();
+            signal.Dispose();
+
+            System.Console.WriteLine($"{stopwatch.ElapsedMilliseconds}: Done");
+            stopwatch.Stop();
             System.Console.ReadLine();
         }
 
-        static void WriteY()
-        {
-            for (int i = 0; i < 1000; i++)
-            {
-                System.Console.Write("y");
-            }
-        }
+//        static void Main(string[] args)
+//        {
+//            bool done = false;
+//
+//            ThreadStart action = () =>
+//            {
+//                if (!done)
+//                {
+//                    done = true;
+//                    System.Console.WriteLine("Done");
+//                }
+//            };
+//
+//            new Thread(action).Start();
+//            action();
+//
+//            System.Console.ReadLine();
+//        }
+
+
+        //        static void Main(string[] args)
+        //        {
+        //            var yThread = new Thread(WriteY);
+        //
+        //            yThread.Start();
+        //            yThread.Name = "y thread";
+        //
+        //            if (yThread.IsAlive)
+        //            {
+        //                System.Console.Write("'Thread y alive'");
+        //
+        //            }
+        //
+        //            for (int i = 0; i < 1000; i++)
+        //            {
+        //                System.Console.Write("x");
+        //            }
+        //
+        //            System.Console.ReadLine();
+        //        }
+        //
+        //        static void WriteY()
+        //        {
+        //            for (int i = 0; i < 1000; i++)
+        //            {
+        //                System.Console.Write("y");
+        //            }
+        //        }
     }
 }
